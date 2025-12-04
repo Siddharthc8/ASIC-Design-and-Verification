82d413d9ce3d7e8fddc43a1eac22110b35bd460c
Siddharth Chandravadivel <siddharthsid2016@gmail.com>
2025-12-04 13:42:49 -0600

    sbd port connections and instantiting in tb_top

--- ASYNC_FIFO_TB/async_fifo_env.sv
+++ ASYNC_FIFO_TB/async_fifo_env.sv
@@ -7,11 +7,21 @@ class async_fifo_env extends uvm_env;
 
 write_agent write_agent_i;
 read_agent read_agent_i;
+fifo_sbd sbd;
 
 function void build_phase(uvm_phase phase);
 super.build_phase(phase);
     write_agent_i = write_agent::type_id::create("write_agent_i", this);
     read_agent_i = read_agent::type_id::create("read_agent_i", this);
+    sbd = fifo_sbd::type_id::create("sbd", this);
+endfunction
+
+function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+
+    write_agent_i.mon.ap_port.connect(sbd.imp_write);
+    read_agent_i.mon.ap_port.connect(sbd.imp_read);
+
 endfunction
 
 endclass
--- ASYNC_FIFO_TB/top_tb.sv
+++ ASYNC_FIFO_TB/top_tb.sv
@@ -23,6 +23,7 @@ import uvm_pkg::*;
 `include "read_cov.sv"
 `include "read_agent.sv"
 
+``include "fifo_sbd.sv"
 `include "async_fifo_env.sv"
 `include "test_lib.sv"
 
