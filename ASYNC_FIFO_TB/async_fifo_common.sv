4a1d244e541ff091ac7b51ad606a67abb0896e13
Siddharthc8 <siddharthsid2016@gmail.com>
2025-12-03 22:28:27 -0600

    added scoreboard coding

--- ASYNC_FIFO_TB/async_fifo_common.sv
+++ ASYNC_FIFO_TB/async_fifo_common.sv
@@ -15,3 +15,11 @@ endfunction
 function new(string name = ""); \
     super.new(name); \
 endfunction
+
+
+class async_fifo_common;
+
+static int num_matches;
+static int num_mismatches;
+
+endclass
---
+++ ASYNC_FIFO_TB/fifo_sbd.sv
@@ -0,0 +1,50 @@
+`uvm_analysis_imp_decl(_write)
+`uvm_analysis_imp_decl(_read)
+// 1.Defines a class uvm_analysis_imp_write 
+// 2. Defines a method called write_write 
+
+
+class fifo_sbd extends uvm_scoreboard;
+`uvm_component_utils(fifo_sbd)
+
+uvm_analysis_imp#(write_tx, fifo_sbd) imp_write;
+uvm_analysis_imp#(read_tx, fifo_sbd) imp_read;
+
+write_tx write_txQ[$];
+read_tx read_txQ[$];
+write_tx write_tx_i;
+read_tx read_tx_i;
+
+`NEW_COMP
+
+function void build_phase(uvm_phase phase);
+super.build_phase(phase);
+imp_write = new("imp_write", this);
+imp_read = new("imp_read", this); 
+endfunction
+
+function void write_write(write_tx tx);
+    write_txQ.push_back(tx);
+endfunction
+
+
+function void write_write(read_tx tx);
+    read_txQ.push_back(tx);
+endfunction
+
+task run_phase(uvm_phase phase);
+forever begin
+    wait ( write_txQ.size() > 0 && read_txQ.size() > 0 );
+    write_tx_i = write_txQ.pop_front();
+
+    if(write_tx_i.data == read_tx_i.data) begin
+        async_fifo_common::num_matches++;
+    end
+    else begin
+        async_fifo_common::num_mismatches++;
+    end
+
+end
+endtask
+
+endclass
