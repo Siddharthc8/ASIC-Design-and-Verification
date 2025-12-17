3029516e25932148661f888c5893f2043af111d1
Siddharth Chandravadivel <siddharthsid2016@gmail.com>
2025-12-17 16:54:16 -0600

    Added AXI_UVC_VG update

--- AXI_UVC_VG/axi_drv.sv
+++ AXI_UVC_VG/axi_drv.sv
@@ -27,6 +27,36 @@ endtask
 task drive_tx(axi_tx tx);
 
     `uvm_info(get_type_name(), "Driving tx", UVM_MEDIUM);
+    if (tr.wr_rd == 1) begin
+        write_addr_phase(tx);
+        write_data_phase(tx);
+        write_resp_phase(tx);
+    end
+    else begin
+        read_addr_phase(tx);
+        read_data_phase(tx);
+    end
+endtask
+
+task write_addr_phase(axi_tx tx);
+
+    
+
+endtask
+
+task write_data_phase(axi_tx tx);
+
+endtask
+
+task write_resp_phase(axi_tx tx);
+
+endtask
+
+task read_addr_phase(axi_tx tx);
+
+endtask
+
+task read_data_phase(axi_tx tx);
 
 endtask
 
