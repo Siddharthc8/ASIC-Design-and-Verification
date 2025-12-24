d81181b19cdebb61155cd99435c855febffcbff2
Siddharth Chandravadivel <siddharthsid2016@gmail.com>
2025-12-24 01:16:36 -0600

    Updated run.sh and added run.do for reference

--- AXI_UVC_VG_BYTE_SBD/run.sh
+++ AXI_UVC_VG_BYTE_SBD/run.sh
@@ -1,2 +1,10 @@
-vcs -sverilog -full64 -debug_access+all -kdb +incdir+../../src +define+UVM_NP_DPI top.sv
-./simv +UVM_TIMEOUT=5000 -l sim.log &
+#vcs -sverilog -full64 -debug_access+all -kdb +incdir+../../src +define+UVM_NO_DPI top.sv
+#./simv +UVM_TIMEOUT=5000 -l sim.log &
+
+vcs -sverilog -full64 +incdir+../../src +define+UVM_NO_DPI top.sv \
++incdir+../hdd2/home/siddharthsid/Sid/AXI_UVC_BYTE_SBD \
+-debug_access+all -kdb \
+-ntbs_opts uvm-1.2 \
++define+UVM_NO_DPI
+
+./simv +UVM_VERBOSITY=UVM_FULL +UVM_TESTNAME="axi_wr_rd_test" -seed=14589 -l sim.log &
