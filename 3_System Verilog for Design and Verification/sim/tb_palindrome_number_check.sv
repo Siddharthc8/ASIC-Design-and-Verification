bb5864cdfb4d5670456825b4cbb1be260668909c
Siddharth Chandravadivel <siddharthsid2016@gmail.com>
2026-02-05 11:07:38 -0600

    added palindrome num_check 2nd method

--- 3_System Verilog for Design and Verification/sim/tb_palindrome_number_check.sv
+++ 3_System Verilog for Design and Verification/sim/tb_palindrome_number_check.sv
@@ -4,15 +4,34 @@ module tb_palindrome_number_check();
         int original, reversed, digit;
 
         if (num < 0) return 0;
-            original = num; reversed = 0;
-            while (num > 0) begin
-                digit = num % 10;
-                reversed = reversed * 10 + digit;
-                num = num / 10;
-            end
 
-        return (original == reversed);
+        original = num; reversed = 0;
+        
+        while (num > 0) begin
+            digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        end
+
+    return (original == reversed);
 
     endfunction 
+
+    //----------------------------------------------------//
+
+    function is_pal_string(int num);
+
+        string num_str;
+
+        num_str = $sformatf("%0d", num);
+        string len = num_str.len();
+
+        for(int i = 0; i < len/2; i++) begin
+            if(num_str[i] != num_str[l-1-i]) return False;
+        end
+
+        return True;
+
+    endfunction
 
 endmodule
